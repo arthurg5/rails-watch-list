a4bbf73dcc06b5a9ae6e5963ec4161b09c2d1290
arthur gumery arthur.gumery5@gmail.com <arthur.gumery5@gmail.com>
2023-11-17 13:56:41 +0100

    &

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = ENV ['djwmjh3zt']
+  config.api_key = ENV ['259183459651981']
+  config.api_secret = ENV ['o-VHzrVmTNGN__9wpglCFynRjiU']
+  config.secure = true
+  config.cdn_subdomain = true
+end
